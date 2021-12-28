574a017205cbb722a947576b2804cfe0b099cf41
Yuliia Sida <77783002+YuliaSwr@users.noreply.github.com>
2021-12-28 20:26:05 +0200

    added spring security

--- src/main/java/app/web/quiz/Response.java
+++ src/main/java/app/web/quiz/Response.java
@@ -1,4 +1,4 @@
-package engine;
+package app.web.quiz;
 
 import lombok.*;
 
--- src/main/java/app/web/quiz/WebQuizController.java
+++ src/main/java/app/web/quiz/WebQuizController.java
@@ -3,6 +3,8 @@ package app.web.quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -13,26 +15,34 @@ import java.util.*;
 @RequestMapping("/api")
 public class WebQuizController {
 
+    @Autowired
     private QuizService quizService;
 
     @Autowired
-    public WebQuizController(QuizService quizService) {
-        this.quizService = quizService;
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public void registerUser(@Valid @RequestBody User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        userRepository.save(user);
     }
 
     @PostMapping("/quizzes/{id}/solve")
-    public engine.Response postAnswer(@PathVariable int id, @RequestBody Map<String, List<Integer>> answer) {
+    public Response postAnswer(@PathVariable int id, @RequestBody Map<String, List<Integer>> answer) {
 
         if (!quizService.existById(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
-        Question question = quizService.getQuestionById(id);
+        Question question = quizService.getById(id);
         List<Integer> rightAnswers = question.getAnswer();
         List<Integer> userAnswers = answer.get("answer");
 
         if ((rightAnswers.isEmpty() && !userAnswers.isEmpty()) || (!rightAnswers.isEmpty() && userAnswers.isEmpty())) {
-            return new engine.Response(false);
+            return new Response(false);
         }
 
         if (!rightAnswers.isEmpty() && !userAnswers.isEmpty()) {
@@ -44,16 +54,18 @@ public class WebQuizController {
                 }
             }
             if (rightAnswers.size() != userAnswers.size() || !isRightAnswer) {
-                return new engine.Response(false);
+                return new Response(false);
             }
         }
-        return new engine.Response(true);
+        return new Response(true);
     }
 
-    @PostMapping("/quizzes")
-    public Question postQuestion(@Valid @RequestBody Question question) {
-        quizService.saveQuestion(question);
-        return quizService.getQuestionById(question.getId());
+    @PostMapping("quizzes")
+    public Question createQuiz(@Valid @RequestBody Question question,
+                               @AuthenticationPrincipal User user) {
+        question.setUser(user);
+        quizService.save(question);
+        return quizService.getById(question.getId());
     }
 
     @GetMapping("/quizzes/{id}")
@@ -61,11 +73,28 @@ public class WebQuizController {
         if (!quizService.existById(id)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
-        return quizService.getQuestionById(id);
+        return quizService.getById(id);
     }
 
-    @GetMapping("/quizzes")
-    public ResponseEntity<List<Question>> getAllQuestions() {
-        return new ResponseEntity<>(quizService.getAllQuestions(), HttpStatus.OK);
+    @GetMapping("quizzes")
+    public List<Question> getQuizzes() {
+        return quizService.getAll();
+    }
+
+    @DeleteMapping("quizzes/{id}")
+    public ResponseEntity<?> deleteQuiz(@PathVariable int id,
+                                        @AuthenticationPrincipal User user) {
+        if (!quizService.existById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        Question question = quizService.getById(id);
+
+        if (question.getUser().getId() == user.getId()) {
+            quizService.delete(question);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
     }
 }
